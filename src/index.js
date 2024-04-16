4369f274bc162641db978f772cccdf815bb271d7
karaciubamikhail <karaciubamikhail@gmail.com>
2024-04-16 12:47:03 +0300

    done 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    return m;
-  }
-  
-  export default {
-    info
-  };
+const ajs = require('@netology-code/ajs');
+
+console.log(ajs.info());
